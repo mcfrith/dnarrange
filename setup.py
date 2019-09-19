85f87711c279888b006d3717fdcccb822d97d556
Martin C. Frith <mcfrith@gmail.com>
2019-09-19 13:47:17 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+commitInfo = "$Format:%d$".strip("( )").split()
+version = commitInfo[commitInfo.index("tag:") + 1].rstrip(",")
+
+setuptools.setup(
+    name="dnarrange",
+    version=version,
+    description='Find rearrangements in "long" DNA reads relative to a genome sequence',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/mcfrith/dnarrange",
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+    ],
+    scripts=[
+        "dnarrange",
+        "dnarrange-merge",
+        "dnarrange-link",
+        "last-multiplot",
+    ],
+)
